629503ae083f54f119965abeb3c3b2515bfa5875
unknown <sashabidardel@gmail.com>
2021-12-16 15:31:29 +0100

    subiendo 3er y 4º test

--- src/Character.php
+++ src/Character.php
@@ -7,11 +7,13 @@ class Character {
    private $level;
    private $alive;
    private $damage;
+   private $heal;
    public function __construct(){
        $this->health = 1000;
        $this->level = 1;
        $this->alive = true;
-       $this->damage = 800;
+       $this->damage = 1000;
+       $this->heal= 200;
    }
   
    /**
@@ -45,15 +47,31 @@ class Character {
    {
       return $this->damage;
    }
+   /**
+    * Get the value of heal
+    */ 
+    public function getHeal()
+    {
+       return $this->heal;
+    }
    public function deal_damage($character,$damage){
     $character->health = $character->health - $damage;
     if ($character->health < 1){
      $character->health = 0;
      $character->alive = false;
     }
-    
-    
-}
+   }
+   public function heal($character,$heal){
+       if ($character->health == 0){
+           $character->alive = false;
+       }
+       $character->health =  $character->health + $heal;
+       if ($character->health > 1000){
+        $character->health=1000;
+       }
+   }
+
+   
 }
 
 ?>
--- tests/CharacterTest.php
+++ tests/CharacterTest.php
@@ -23,8 +23,24 @@ class CharacterTest extends TestCase {
     public function test_deal_damage(){
 		$character1 = new character;
 		$character2 = new character;
-		$resultEspected = 200;
+		$resultEspected = 0;
 		$character1->deal_damage($character2,$character1->getDamage());
+		$this->assertEquals($resultEspected, $character2->getHealth());
+	}
+	public function test_deal_demage_health_equal_zero(){
+		$character1 = new character;
+		$character2 = new character;
+		$resultEspected = 0;
+		$alive = false;
+		$character1->deal_damage($character2,$character1->getDamage());
+		$this->assertEquals($resultEspected, $character2->getHealth());
+		$this->assertEquals($alive, $character2->getAlive());
+	}
+	public function test_heal(){
+		$character1 = new character;
+		$character2 = new character;
+		$resultEspected = 1000;
+		$character1->heal($character2,$character1->getHeal());
 		$this->assertEquals($resultEspected, $character2->getHealth());
 	}
 }
